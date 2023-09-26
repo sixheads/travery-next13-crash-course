8bcc60e7926f6935adcb6178fecc380ac511e8a8
John Fry <john@sixheads.com>
2023-09-26 10:01:51 +1000

    Convert to client component. Add state and fetch for courses. Also include Loading component

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,32 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import LoadingPage from './loading';
 import Courses from './components/Courses';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
       <h1>Welcome to Traversy Media</h1>
-      <Courses />
+      <Courses courses={courses} />
     </>
   );
 };
